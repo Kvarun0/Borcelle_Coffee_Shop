dda479a3105c2529e249d8758d424474e9d61d23
Kvarun0 <varunkapadiya03@gmail.com>
2023-12-04 18:10:21 +0530

    complete header section

--- script/script.js
+++ script/script.js
@@ -1,9 +1,41 @@
 
 //for navbar menu btn 
 
+//for menu btn activation
 let navbar=document.querySelector(".navbar");
 
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('.active');
+    navbar.classList.toggle('active');
+    
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+//for cart item button
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+//for search button activation
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () =>{
+    searchForm.classList.toggle('active');
+
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
